615adbcba9d7cc998b0d49d460922679542ebd37
Eniola Samuel <theeniolasamuel@gmail.com>
2025-05-09 11:28:06 +0100

    first commit

---
+++ lib/code-parser.ts
@@ -0,0 +1,20 @@
+export function parseCode(code: string): number[] {
+    if (!code.startsWith("*465") || !code.endsWith("#")) {
+        return []
+    }
+
+
+    return code
+        .slice(4, code.length - 1) // Remove *465 and #
+        .split("*")
+        .filter((item) => item !== "")
+        .map(Number)
+}
+
+export function generateCode(path: number[]): string {
+    if (path.length === 0) {
+        return "*465#"
+    }
+
+    return `*465*${path.join("*")}#`
+}
